0064e9f06f0ddbac533ec7bebb5f0567f38d8b9a
redqueen <chaimaa.ouichoul@gmail.com>
2017-03-06 20:41:47 +0000

    model 'Post' created

---
+++ koramap/app/src/main/java/com/android/korama/model/Post.java
@@ -0,0 +1,85 @@
+package com.android.korama.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by Chaimaa on 06/03/2017.
+ */
+
+public class Post implements Serializable {
+    private String title;
+    private String content;
+    private String author;
+    private Date dt;
+    private int views;
+    private String source;
+    private String imageUrl; //full
+    private String category;
+
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDt() {
+        return dt;
+    }
+
+    public void setDt(Date dt) {
+        this.dt = dt;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getImage_url() {
+        return imageUrl;
+    }
+
+    public void setImage_url(String image_url) {
+        this.imageUrl = image_url;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
